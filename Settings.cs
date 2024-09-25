191e88f04c1451b3ce184b071bd42a312ca42fa4
Mike <matthews.r.mike@gmail.com>
2024-09-25 18:58:18 +0200

    Add setting for testing

--- Settings.cs
+++ Settings.cs
@@ -8,10 +8,13 @@ namespace FASTER
     public class Settings : ModSettings, IDrawable
     {
         // [Draw(DrawType.)]
-        
+
+        //[Draw(DrawType.Toggle)]
+        public bool showMarkers;
+
         [Header("Debug")]
         [Draw(DrawType.Toggle)]
-        public bool showMarkers;
+        public bool testMaxEffect;
         [Draw(DrawType.Toggle)]
         public bool disableInfoLogs;
 
