22d350a60c1d1e5613cfe2c9a9f09d3c2de84a2e
Mike <matthews.r.mike@gmail.com>
2024-09-25 18:59:49 +0200

    Add bootstrap into car camera

--- Patches.cs
+++ Patches.cs
@@ -2,19 +2,40 @@ using HarmonyLib;
 
 namespace FASTER
 {
-	// Patch model
-	// [HarmonyPatch(typeof(), nameof())]
-	// [HarmonyPatch(typeof(), MethodType.)]
-	// static class type_method_Patch
-	// {
-	// 	static void Prefix()
-	// 	{
-	// 		//
-	// 	}
+    // Patch model
+    // [HarmonyPatch(typeof(), nameof())]
+    // [HarmonyPatch(typeof(), MethodType.)]
+    // static class type_method_Patch
+    // {
+    // 	static void Prefix()
+    // 	{
+    // 		//
+    // 	}
 
-	// 	static void Postfix()
-	// 	{
-	// 		//
-	// 	}
-	// }
+    // 	static void Postfix()
+    // 	{
+    // 		//
+    // 	}
+    // }
+
+    [HarmonyPatch(typeof(CarCameras))]
+    static class SpeedEffectManager
+    {
+        // UIManager.Instance.PanelManager.mainCamera
+        // get camera object
+
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void StartPostfix()
+        {
+            Main.Log("StartPostfix");
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void UpdatePostfix()
+        {
+            Main.Log("UpdatePostfix");
+        }
+    }
 }
