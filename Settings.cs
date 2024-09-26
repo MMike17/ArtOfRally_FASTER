89c42182d1aca6885a089db50d09bbd0393c76af
Mike <matthews.r.mike@gmail.com>
2024-09-26 14:56:46 +0200

    Add settings names

--- Settings.cs
+++ Settings.cs
@@ -14,55 +14,64 @@ namespace FASTER
         }
 
         [Header("General")]
-        [Draw(DrawType.Slider, Min = 0, Max = 150)]
+        [Draw("Min speed threshold", DrawType.Slider, Min = 0, Max = 150)]
         public int minSpeedThreshold = 80;
-        [Draw(DrawType.Slider, Min = 50, Max = 200)]
+        [Draw("Max speed threshold", DrawType.Slider, Min = 50, Max = 200)]
         public int maxSpeedThreshold = 180;
         [Space]
-        [Draw(DrawType.Slider, Min = 0.5f, Max = 7, Precision = 1)]
+        [Draw("Effect up speed", DrawType.Slider, Min = 0.5f, Max = 7, Precision = 1)]
         public float effectUpSpeed = 3;
-        [Draw(DrawType.Slider, Min = 1, Max = 7, Precision = 1)]
+        [Draw("Effect down speed", DrawType.Slider, Min = 1, Max = 7, Precision = 1)]
         public float effectDownSpeed = 4.5f;
+        [Space]
+        [Draw("Reset values", DrawType.Toggle)]
+        public bool resetValues = false;
 
         [Header("Lens distortion")]
-        [Draw(DrawType.Toggle)]
+        [Draw("Enable lens distortion", DrawType.Toggle)]
         public bool enableLensDistortion = true;
-        [Draw(DrawType.Slider, VisibleOn = "enableLensDistortion|true", Min = 30, Max = 90)]
+        [Draw("Distortion intensity", DrawType.Slider, VisibleOn = "enableLensDistortion|true", Min = 30, Max = 90)]
         public int distortionIntensity = 40;
 
         [Header("Chromatic aberration")]
-        [Draw(DrawType.Toggle)]
+        [Draw("Enable chromatic aberration", DrawType.Toggle)]
         public bool enableChromaticAberration = true;
-        [Draw(DrawType.Toggle, VisibleOn = "enableChromaticAberration|true")]
+        [Draw("Aberration fast mode", DrawType.Toggle, VisibleOn = "enableChromaticAberration|true")]
         public bool aberrationFastMode = false;
-        [Draw(DrawType.Slider, VisibleOn = "enableChromaticAberration|true", Min = 0.5f, Max = 3, Precision = 1)]
+        [Draw("Aberration intensity", DrawType.Slider, VisibleOn = "enableChromaticAberration|true", Min = 0.5f, Max = 3, Precision = 1)]
         public float aberrationIntensity = 1;
 
         [Header("Bloom")]
-        [Draw(DrawType.Toggle)]
+        [Draw("Enable bloom", DrawType.Toggle)]
         public bool enableBloom = true;
-        [Draw(DrawType.Slider, VisibleOn = "enableBloom|true", Min = 0.8f, Max = 0.2f, Precision = 1)]
+        [Draw("Bloom threshold", DrawType.Slider, VisibleOn = "enableBloom|true", Min = 0.8f, Max = 0.2f, Precision = 1)]
         public float bloomThreshold = 0.4f;
-        [Draw(DrawType.Slider, VisibleOn = "enableBloom|true", Min = 0.5f, Max = 5, Precision = 1)]
+        [Draw("Bloom Intensity", DrawType.Slider, VisibleOn = "enableBloom|true", Min = 0.5f, Max = 5, Precision = 1)]
         public float bloomIntensity = 3f;
 
         [Header("Vignette")]
-        [Draw(DrawType.Toggle)]
+        [Draw("Enable vignette", DrawType.Toggle)]
         public bool enableVignette = true;
-        [Draw(DrawType.Slider, VisibleOn = "enableVignette|true", Min = 0.15f, Max = 0.5f, Precision = 1)]
+        [Draw("Vignette intensity", DrawType.Slider, VisibleOn = "enableVignette|true", Min = 0.15f, Max = 0.5f, Precision = 1)]
         public float vignetteIntensity = 0.3f;
 
         [Header("Debug")]
-        [Draw(DrawType.Toggle)]
-        public bool testMaxEffect;
-        [Draw(DrawType.Toggle)]
-        public bool disableInfoLogs;
+        [Draw("Test max effect", DrawType.Toggle)]
+        public bool testMaxEffect = false;
+        [Draw("Disable info logs", DrawType.Toggle)]
+        public bool disableInfoLogs = true;
 
         public override void Save(ModEntry modEntry) => Save(this, modEntry);
 
         public void OnChange()
         {
             // TODO : Do validation for minSpeedThreshold and maxSpeedThreshold
+
+            if (resetValues)
+            {
+                // TODO : Reset values to default values
+                resetValues = false;
+            }
         }
     }
 }
